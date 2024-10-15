b70a026d491c1b4bca2cd122841a7b176af3ab7b
Iakubovskiy <diman.150106@gmail.com>
2024-10-15 19:41:05 +0300

    RSS-3 registration/autentification plus model fix

--- backend/user-auth-service/user-auth-service/user_auth_service/users/services/AuthService.py
+++ backend/user-auth-service/user-auth-service/user_auth_service/users/services/AuthService.py
@@ -1,10 +1,11 @@
 from urllib.request import Request
-from django.contrib.auth.models import User, Group
-from user_auth_service.users.models import User
-from user_auth_service.users.services.abstracts.auth_base import AuthServiceBase
+from django.contrib.auth.models import Group
+from ..models import User
+from ..services.abstracts.auth_base import AuthServiceBase
 from rest_framework import status
-from rest_framework_simplejwt.views import TokenObtainPairView
-from djoser.serializers import UserCreateSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from ..serializers import UserCreateSerializer
+
 
 class AuthService(AuthServiceBase):
 
@@ -14,21 +15,18 @@ class AuthService(AuthServiceBase):
         user = User.objects.filter(email=email).first()
 
         if user is not None and user.check_password(password):
-            request = Request(self)
-            request.data['email'] = email
-            request.data['password'] = password
-            token_view = TokenObtainPairView.as_view()
-            response = token_view(request)
-            if response.status_code == status.HTTP_200_OK:
-                token_data = response.data
-                return {
-                    "status_code": status.HTTP_200_OK,
-                    "access": token_data['access'],
-                    "refresh": token_data['refresh'],
-                    "user_id": user.id,
-                    "username": user.username,
-                    "groups": [group.name for group in user.groups.all()]
-                }
+            refresh = RefreshToken.for_user(user)
+            access_token = str(refresh.access_token)
+            refresh_token = str(refresh)
+
+            return {
+                "status_code": status.HTTP_200_OK,
+                "access_token": access_token,
+                "refresh_token": refresh_token,
+                "user_id": user.id,
+                "username": user.username,
+                "groups": [group.name for group in user.groups.all()]
+            }
         return {
             "status_code": status.HTTP_401_UNAUTHORIZED,
             "message": "Invalid email or password."
@@ -36,7 +34,7 @@ class AuthService(AuthServiceBase):
 
     def register(self, data: dict) -> dict:
         serializer = UserCreateSerializer(data=data)
-        if serializer.is_valid():
+        if serializer.is_valid() and data.get('role') is not None:
             user = serializer.save()
 
             role = data.get('role')
