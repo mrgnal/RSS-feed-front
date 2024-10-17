579491e876609790970ce2773a7cff8d66d2deb2
Iakubovskiy <diman.150106@gmail.com>
2024-10-17 17:10:39 +0300

    RSS-6 google login

--- backend/user-auth-service/user-auth-service/user_auth_service/users/urls.py
+++ backend/user-auth-service/user-auth-service/user_auth_service/users/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
-from .views import LoginView, RegisterView
+from django.urls import path, include
+from .views import LoginView, RegisterView,GoogleAuthInitView,GoogleAuthCallbackView
 
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('google/login/', GoogleAuthInitView.as_view(), name='google_auth_init'),
+    path('google/login/callback/', GoogleAuthCallbackView.as_view(), name='google_auth_callback'),
 ]
