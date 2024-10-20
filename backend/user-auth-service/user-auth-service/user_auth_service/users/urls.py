609dfef6ef9e31a7e2ea64b9366783c8eadde0ff
Iakubovskiy <diman.150106@gmail.com>
2024-10-20 04:16:26 +0300

    RSS-7 editing and deliting for user

--- backend/user-auth-service/user-auth-service/user_auth_service/users/urls.py
+++ backend/user-auth-service/user-auth-service/user_auth_service/users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
-from .views import LoginView, RegisterView,GoogleAuthInitView,GoogleAuthCallbackView, change_password
+from .views import LoginView, RegisterView, GoogleAuthInitView, GoogleAuthCallbackView, change_password, \
+    UserProfileUpdateView, UserProfileDeleteView
 from . import views
 
 
@@ -11,4 +12,6 @@ urlpatterns = [
     path('activate/<uidb64>/<token>', views.activate, name='activate'),
     path('change_password/', change_password, name='change_password'),
     path('password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
+    path('change_account_data/', UserProfileUpdateView.as_view(), name='change_data'),
+    path('delete_profile/', UserProfileDeleteView.as_view(), name='delete_profile'),
 ]
