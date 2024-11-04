e3a1ea5c9618cf902809cfa822412a01f06ec49b
Denys Zherdetskyi <144510034+dizzzyy0@users.noreply.github.com>
2024-11-05 00:56:38 +0200

    add cors for Next.js domain

--- backend/user-auth-service/user-auth-service/user_auth_service/user_auth_service/settings.py
+++ backend/user-auth-service/user-auth-service/user_auth_service/user_auth_service/settings.py
@@ -48,6 +48,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -89,6 +90,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,7 +125,10 @@ ACCOUNT_AUTHENTICATION_METHOD = "email"
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = "none"
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 
+]
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
