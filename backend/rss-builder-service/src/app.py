977246ac697a5996bcfd01a6e5622ea7e1c03956
Vitalii <alexvitalik65@gmail.com>
2024-11-07 18:43:36 +0200

    "Page fetch API"

---
+++ backend/rss-builder-service/src/app.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from parser import fetch_full_page
+
+app = FastAPI()
+
+@app.post('/api/fetch_page/')
+def get_source_page(url: str):
+    html_content = fetch_full_page(url)
+    return JSONResponse(content={'html_content': html_content})
+
